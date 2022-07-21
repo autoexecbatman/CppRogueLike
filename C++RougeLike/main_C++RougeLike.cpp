6fd7d746234732caaf976906f3a94fcb8239cb1d
autoexecbatman <91700559+autoexecbatman@users.noreply.github.com>
2022-07-21 11:56:43 +0300

    stage 1 working

--- C++RougeLike/main_C++RougeLike.cpp
+++ C++RougeLike/main_C++RougeLike.cpp
@@ -1,33 +1,25 @@
 // Debug needs to be set to x86
+#include <curses.h>
 #include "main_C++RougeLike.h"
-//====
-#define GRASS_PAIR     1
-#define EMPTY_PAIR     1
-#define WATER_PAIR     2
-#define MOUNTAIN_PAIR  3
-#define NPC_PAIR       4
-#define PLAYER_PAIR    5
+
 //====
 Map::Map(int height, int width) :  height(height) , width(width)
 {
     tiles = new Tile[height * width];
+    setWall(22, 30);
+    setWall(22, 40);
 
-    //Bsp bsp(0, 0, height, width);
-
-    //bsp.mysplitRecursive(NULL, 8, ROOM_MAX_SIZE, ROOM_MAX_SIZE, 1.5f, 1.5f);
-
+    ////Bsp bsp(0, 0, height, width);
+    ////bsp.mysplitRecursive(NULL, 8, ROOM_MAX_SIZE, ROOM_MAX_SIZE, 1.5f, 1.5f);
+    ////BspListener listener(*this);
+    ////bsp.traverseInvertedLevelOrder(&listener, NULL);
+    //TCODBsp bsp(0, 0, width, height);
+    ////TCODBsp* ptrbsp = 0;
+    //
+    //bsp.splitRecursive(NULL, 8, ROOM_MAX_SIZE, ROOM_MAX_SIZE, 1.5f, 1.5f);
     //BspListener listener(*this);
     //bsp.traverseInvertedLevelOrder(&listener, NULL);
 
-    TCODBsp bsp(0, 0, width, height);
-    /*TCODBsp* ptrbsp = 0;*/
-    
-    bsp.splitRecursive(NULL, 8, ROOM_MAX_SIZE, ROOM_MAX_SIZE, 1.5f, 1.5f);
-    BspListener listener(*this);
-    bsp.traverseInvertedLevelOrder(&listener, NULL);
-
-    //setWall(22, 30);
-    //setWall(22, 40);
 }
 
 Map::~Map()
@@ -47,162 +39,35 @@ void Map::setWall(int y, int x)
 
 void Map::render() const
 {
-    int y = 0;
-    int x = 0;
-    //green
-    static const int darkWall = 1;
-    //blue
-    static const int darkGround = 3;
-    /*mvchgat(y, x, 1, A_NORMAL, isWall(y, x) ? darkWall : darkGround, NULL);*/
+    static const int darkWall = 1;//green
+    static const int darkGround = 2;//blue
 
-    
     for (int x = 0; x < width; x++)//if x smaller then map width
     {
-        
         for (int y = 0; y < height; y++)//if y smaller then map width
         {
             //TCODConsole::root->setCharBackground(x, y,
             //    isWall(x, y) ? darkWall : darkGround);
-            /*printw("w");*/
-            
-            /*mvchgat(y,x,-1, A_NORMAL,isWall(y,x) ? darkWall : darkGround, NULL);*/
+            //mvchgat(y,x,-1, A_NORMAL,isWall(y,x) ? darkWall : darkGround, NULL);
             mvchgat(y,x,1, A_NORMAL,isWall(y,x) ? darkWall : darkGround, NULL);
-            //addch('#');
-            //setWall(y,x);
-
-        }
-    }
-    
-    //for (int i = 0; i < x; i++)
-    //{
-    //    if (isWall(y, x))
-    //    {
-    //        addch('#');
-    //    }
-    //    else
-    //    {
-    //        addch('_');
-    //    }
-    //}
-
-}
-
-void Map::dig(int y1, int x1, int y2, int x2)
-{
-
-        //int tiley = y1+20;
-        //int tilex = x1+20;
-        
-
-        //tiles[tilex + tiley * width].canWalk = true;
-        //printw("x1:%u|", x1);
-        //printw("y1:%u|\n", x1);
-
-
-        //swap x
-        if (x2 < x1)
-        {
-            //printw("x1:%u|", x1);
-            //printw("y1:%u", x1);
-            //printw("num:%u\n", iter);
-            int tmp = x2;
-            x2 = x1;
-            x1 = tmp;
-        }
-        //printw("x1:%u~", x1);
-        //printw("y1:%u|", x1);
-
-        //swap y
-        if (y2 < y1)
-        {
-            int tmp = y2;
-            y2 = y1;
-            y1 = tmp;
-        }
-
-        for (int tilex = x1; tilex <= x2; tilex++)
-        {
-            for (int tiley = y1; tiley <= y2; tiley++)
-            {
-                tiles[tilex + tiley * width].canWalk = true;
-            }
-        }
-        
-        //for (int tiley = y1; tiley <= y2; tiley++)
-        //{
-        //    for (int tilex = x1; tilex <= x2; tilex++)
-        //    {
-        //        tiles[tilex + tiley * width].canWalk = true;
-        //    }
-        //}
-}
-
-void Map::createRoom(bool first, int y1, int x1, int y2, int x2)
-{
-    /*Engine engine;*/
-
-    dig(y1, x1, y2, x2);
-
-    if (first)
-    {
-        ////put the player in the first room
-        //engine.player->y = (y1 + y2) / 2;
-        //engine.player->x = (x1 + x2) / 2;
-
-        engine.player->y = x1 + 1;
-        engine.player->x = y1 + 1;
-
-    }
-    else
-    {
-        //TCODRandom random;
-        //std::default_random_engine* generator;
-        //std::uniform_int_distribution<int> distribution(1, 6);
-        //auto dice = std::bind(distribution, generator);
-        //auto rng = dice;
-        
-        /*sp::StaticRandomInitializer* rng;*/
-        //sp::StaticRandomInitializer::StaticRandomInitializer()* rng = 0;
-        /*sp::StaticRandomInitializer::StaticRandomInitializer();*/
-        //auto* rng = sp::irandom;
-
-
-
-        /*srand(time(NULL));*/
-        
-        //int* ptr_rng;
-        //int* ptr_rng = &rngrand;
-
-        
-        if (int rng = rand() % 4 == 0)
-        {
-            engine.actors.push_back(
-                new Actor(
-                (x1 + x2) / 2,
-                (y1 + y2) / 2,
-                '@',
-                4
-                )
-            );
         }
     }
 }
-//====
+
 Engine::Engine()
 {
-    //int map_y = 80;
-    //int map_x = 45;
+    /*TCODConsole::initRoot(80, 50, "libtcod C++ tutorial", false);*/
     initscr();
     start_color();
-    
     player = new Actor(25, 40, '@',3);
     actors.push_back(player);
-    /*actors.push_back(new Actor(13, 60,  '@', 3));*/
-    map = new Map(30, 120);
+    /*map = new Map(30, 120);*/
+    map = new Map(80, 45);
 }
 
 Engine::~Engine() 
 {
+    /*actors.clearAndDelete();*/
     actors.clear();
     actors.~vector();
     delete map;
@@ -210,37 +75,53 @@ Engine::~Engine()
 
 void Engine::update()
 {
-    
-    /*mvaddch(1,1,"%c",player->y);*/
-
+    ////DEBUG
     //mvprintw(1,1,"player->y:%u",player->y);
     //mvprintw(2,1,"player->x:%u",player->x);
-    //printw("?");
-    int key = getch();
 
+    //TCOD_key_t key;
+    //TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL);
+    int key = getch();
     switch (key)
     {
+    //case TCODK_UP:
+    //    if (!map->isWall(player->x, player->y - 1)) {
+    //        player->y--;
+    //    }
+    //    break;
     case UP:
         if (!map->isWall(player->y - 1, player->x))
         {
             player->y--;
         }
         break;
-
+    //case TCODK_DOWN:
+    //    if (!map->isWall(player->x, player->y + 1)) {
+    //        player->y++;
+    //    }
+    //    break;
     case DOWN:
         if (!map->isWall(player->y + 1, player->x))
         {
             player->y++;
         }
         break;
-
+    //case TCODK_LEFT:
+    //    if (!map->isWall(player->x - 1, player->y)) {
+    //        player->x--;
+    //    }
+    //    break;
     case LEFT:
         if (!map->isWall(player->y , player->x - 1))
         {
             player->x--;
         }
         break;
-
+    //case TCODK_RIGHT:
+    //    if (!map->isWall(player->x + 1, player->y)) {
+    //        player->x++;
+    //    }
+    //    break;
     case RIGHT:
         if (!map->isWall(player->y,player->x + 1))
         {
@@ -248,14 +129,14 @@ void Engine::update()
         }
         break;
 
-    default:
-        break;
+    default:break;
     }
 }
 
 void Engine::render() 
 {    
-    clear();//curses clear()
+    //TCODConsole::root->clear();
+    clear();
     // draw the map
     map->render();
 
@@ -271,6 +152,8 @@ Actor::Actor(int y, int x, int ch, int col) : y(y), x(x), ch(ch), col(col)
 
 void Actor::render() const
 {
+    //TCODConsole::root->setChar(x, y, ch);
+    //TCODConsole::root->setCharForeground(x, y, col);
     attron(COLOR_PAIR(col));
     mvaddch(y, x, ch);
     attroff(COLOR_PAIR(col));
@@ -279,21 +162,26 @@ void Actor::render() const
 
 int main()
 {
+    Engine engine;
 //====
-    init_pair(1, COLOR_YELLOW, COLOR_GREEN);
-    init_pair(2, COLOR_CYAN, COLOR_BLUE);
-    init_pair(3, COLOR_BLACK, COLOR_WHITE);
-    init_pair(4, COLOR_RED, COLOR_MAGENTA);
-    init_pair(5, COLOR_RED, COLOR_YELLOW );
+#define GRASS_PAIR     1
+#define EMPTY_PAIR     1
+#define WATER_PAIR     2
+#define MOUNTAIN_PAIR  3
+#define NPC_PAIR       4
+#define PLAYER_PAIR    5
 //====
-        /*bool quit = false;*/
-    while (true/*!engine.quit == true*/)
+        init_pair(1, COLOR_YELLOW, COLOR_GREEN);
+        init_pair(2, COLOR_CYAN, COLOR_BLUE);
+        init_pair(3, COLOR_BLACK, COLOR_WHITE);
+        init_pair(4, COLOR_RED, COLOR_MAGENTA);
+        init_pair(5, COLOR_RED, COLOR_YELLOW );
+//====
+    while (true)
     {
-        /*initscr();*/
-
         engine.update();
         engine.render();
-        refresh();
+        refresh();//TCODConsole::flush();
     }
     return 0;
 }
--- C++RougeLike/main_C++RougeLike.h
+++ C++RougeLike/main_C++RougeLike.h
@@ -1,11 +1,10 @@
 #pragma once
-
 //#include "mybsp.h"
 #include "libtcod.hpp"
-#include <curses.h>
 
-static const int ROOM_MAX_SIZE = 12;
-static const int ROOM_MIN_SIZE = 6;
+
+//static const int ROOM_MAX_SIZE = 12;
+//static const int ROOM_MIN_SIZE = 6;
 
 enum CONTROLS 
 {
@@ -15,7 +14,7 @@ enum CONTROLS
 struct Tile 
 {
 	bool canWalk; // can we walk through this tile?
-	Tile() : canWalk(false) {}
+	Tile() : canWalk(true) {}
 };
 
 class Map 
@@ -25,148 +24,33 @@ public:
 
 	Map(int height, int width);
 	~Map();
-
 	bool isWall(int y, int x) const;
 	void render() const;
 protected:
 	Tile* tiles;
 
-	friend class BspListener;
-
-	void dig(int y1,int x1, int y2,int x2);
-	void createRoom(bool first, int y1, int x1, int y2, int x2);
 	void setWall(int y, int x);
+	//friend class BspListener;
+
+	//void dig(int y1,int x1, int y2,int x2);
+	//void createRoom(bool first, int y1, int x1, int y2, int x2);
 };
 
-//class BspListener_2 : public BspCallback
-//{
-//private:
-//	Map& map; // a map to dig
-//	int roomNum; // room number
-//	int lasty = 0, lastx = 0; // center of the last room
-//
-//public:
-//	BspListener(Map& map) : map(map), roomNum(0) {}
-//	/*bool visitNode();*/
-//	
-//	bool visitNode(Bsp* node, void* userData)
-//	{
-//		if (node->isLeaf())
-//		{
-//			int y, x, h, w;
-//			// dig a room
-//
-//			TCODRandom* rng = TCODRandom::getInstance();
-//
-//			h = rng->getInt(6, node->h - 2);
-//			w = rng->getInt(6, node->w - 2);
-//			y = rng->getInt(node->y + 1, node->y + node->h - h - 1);
-//			x = rng->getInt(node->x + 1, node->x + node->w - w - 1);
-//
-//			map.createRoom(roomNum == 0, y, x, y + h - 1, x + w - 1);
-//
-//			if (roomNum != 0)
-//			{
-//				// dig a corridor from last room
-//				
-//				map.dig(lasty, x + w / 2, y + h / 2, x + w / 2);
-//				map.dig(lasty, lastx, lasty, x + w / 2);
-//			}
-//			lastx = x + w / 2;
-//			lasty = y + h / 2;
-//			roomNum++;
-//		}
-//		return true;
-//	}
-//};
-
-class BspListener : public ITCODBspCallback 
-{
-private:
-	Map& map; // a map to dig
-	int roomNum; // room number
-	int lastx, lasty; // center of the last room
-public:
-	BspListener(Map& map) : map(map), roomNum(0) {}
-
-	bool visitNode(TCODBsp* node, void* userData)
-	{
-
-		if (node->isLeaf())
-		{
-			//printw("isLeaf\n");
-			//printw("%u",node->isLeaf());
-			
-			int x, y, w, h;
-			// dig a room
-			TCODRandom* rng = TCODRandom::getInstance();
-			w = rng->getInt(ROOM_MIN_SIZE, node->w - 2);//random int from min size to width - 2
-			h = rng->getInt(ROOM_MIN_SIZE, node->h - 2);//random int from min size to height - 2
-			x = rng->getInt(node->x + 1, node->x + node->w - w - 1);//from node x + 1 to node x + node width - width - 1
-			y = rng->getInt(node->y + 1, node->y + node->h - h - 1);//from node y + 1 to node x + node height - width - 1
-
-			//DEBUG rng
-			printw("w = %u||",w);
-			printw("h = %u||",h);
-			printw("x = %u||",x);
-			printw("y = %u||",y);
-			printw("roomnum = %u\n", roomNum);
-
-			map.createRoom
-			(
-				roomNum == 0,
-				x,
-				y,
-				x + w - 1,
-				y + h - 1
-			);
-
-
-
-			//DEBUG createRoom
-
-
-			if (roomNum != 0) 
-			{
-				// dig a corridor from last room
-
-				map.dig(
-					lasty,
-					lastx,
-					lasty,
-					x + w / 2
-				);
-				map.dig(
-					lasty,
-					x + w / 2,
-					y + h / 2,
-					x + w / 2
-				);
-			}
-			lastx = x + w / 2;
-			lasty = y + h / 2;
-			roomNum++;
-		}
-		return true;
-	}
-};
 
 class Actor
 {
 public:
 	int y, x;//position on map
 	int ch;//ascii code
-	int col; //color
+	int col; //TCODColor col;//color
 
-	Actor(int y ,int x, int ch, int col);
+	Actor(int y ,int x, int ch, int col/*const TCODColor& col */);
 	void render() const;
 };
 
 class Engine
 {
 public:
-	/*Engine* engine;*/
-	/*bool quit = false;*/
 	std::vector<Actor*> actors;
 	Actor* player;
 	Map* map;
@@ -176,6 +60,4 @@ public:
 	void update();
 	void render();
 };
-
-Engine engine;
-//extern Engine engine;
+/*extern Engine engine;*/
