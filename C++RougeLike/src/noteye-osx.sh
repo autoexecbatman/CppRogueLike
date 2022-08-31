061b69a5986973ec464b7a84d3755aea58410bc0
autoexecbatman <91700559+autoexecbatman@users.noreply.github.com>
2022-08-31 22:47:08 +0300

    Delete C++RougeLike/src directory

--- C++RougeLike/src/noteye-osx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export CFGDIR=$HOME/.config/noteye/
-mkdir -p $CFGDIR
-cd "${0%/*}"
-cd ../Resources
-../MacOS/NotEye -f $CFGDIR/hydra.sav -b $CFGDIR/hydra.sav -t $CFGDIR/hydralog.txt -g $CFGDIR/hydrascore.sav -N -U $CFGDIR
