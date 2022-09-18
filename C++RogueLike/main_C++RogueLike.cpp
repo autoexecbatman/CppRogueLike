55a7ebe8272471ac47255b585fcbf6eaf1d96fbe
autoexecbatman <91700559+autoexecbatman@users.noreply.github.com>
2022-09-18 17:12:42 +0300

    changed run to boolean and added TODOs

--- C++RogueLike/main_C++RogueLike.cpp
+++ C++RogueLike/main_C++RogueLike.cpp
@@ -3,60 +3,75 @@
 //we are making a roguelike game
 //using the curses library
 //and the classes from doryen TCOD libtcod library without using the TCOD console
+
 #include <iostream>
 #include <curses.h>
 
-#include "main.h"
+#include "main.h" // TODO : find a better way to include multiple headers.
 #include "Colors.h"
 #include "Window.h"
 
+//==ENGINE==
+// an instance of the Engine class.
+Engine engine(30, 120); // TODO : check what is going on with the width/height of the console window.
 
-
-//====
-//an instance of the Engine class
-Engine engine(30, 120);
-
+// TODO : find an appropriate location for this function.
 void wizardeye()
 {
 	for (const auto& actor : engine.actors)
 	{
-		//print the actor's name
+		// print the actor's name
 		mvprintw(actor->y, actor->x, actor->name);
 	}
 }
 
-
-//====
-//this is the main function where the game will run
+//==MAIN_FUNCTION==
+// This is the main function where the game will run.
 int main()
 {
-	//====
+	//==COLORS==
 	//initialize the color pairs
 	Colors colors;
 	colors.my_init_pair();
 
+	//==WINDOW==
+	// create a window object
 	Window window;
 	
+	//==ENGINE==
+	// load the game if not init new one
 	engine.load();
-	//====
-	//main game loop
+
+	//==GAME_LOOP==
+	// main game loop
 	int countLoop = 0;
-	while (engine.run == 1 && !engine.player->destructible->isDead())
+	while (engine.run == true && !engine.player->destructible->isDead())
 	{
-
-		std::clog << "Running..." << countLoop << std::endl;
-		engine.update(); // update the map
-		engine.render(); // render the map
-		refresh(); // refresh the window
-
-		window.create_window(3, 0, 0, engine.player->name);
-		countLoop++;
-
-		if (engine.run == 0)
+		// checks if 'q' was not pressed
+		if (engine.run == false)
 		{
 			break;
 		}
+		//==DEBUG==
+		std::clog << "Running..." << countLoop << std::endl;
+
+		//==ENGINE_FUNCTIONS==
+		// TODO : Clear and redraw, then present, then wait for input.  In that order.
+		// example : while (true) { clear(); refresh(); getch(); }
+
+		engine.update(); // update map and actors positions and other stuff
+		engine.render(); // render map and actors to the screen
+		
+		// TODO : move to the appropriate location.
+		refresh(); // refresh the window
+
+		// TODO : decide if you are going to use this class.
+		window.create_window(3, 0, 0, engine.player->name);
+
+		countLoop++;
 	}
+	//==ENGINE==
+	// save the game after the loop breaks
 	engine.save();
 
 	return 0;
