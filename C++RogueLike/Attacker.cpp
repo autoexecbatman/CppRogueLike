b0e0a636f6a7058cd1fefed4ad698354afc82929
autoexecbatman <91700559+autoexecbatman@users.noreply.github.com>
2022-10-27 14:57:24 +0300

    added colored message

--- C++RogueLike/Attacker.cpp
+++ C++RogueLike/Attacker.cpp
@@ -13,9 +13,10 @@ void Attacker::attack(Actor* owner, Actor* target)
 
 	if (target->destructible && !target->destructible->is_dead())
 	{
-
+		// if the target takes damage then a message is displayed
 		if (power - target->destructible->defense > 0)
 		{
+			// damage message
 			engine.gui->log_message(
 				LIGHT_WALL_PAIR, // color
 				"%s attacks %s\n for %g hit points.\n", // message
@@ -23,9 +24,25 @@ void Attacker::attack(Actor* owner, Actor* target)
 				target->name,
 				power - target->destructible->defense
 			);
+			
+			// rewrite `attron(COLOR_PAIR(Actor::col));` so it uses the color of the actor
+			attron(COLOR_PAIR(owner->col));
+			mvprintw(0, 0, "%s", owner->name);
+			attroff(COLOR_PAIR(owner->col));
+			
+			int ownerNameLen = strlen(owner->name);
+			const char* attacksThe = " attacks the ";
+			int attacksTheLen = strlen(attacksThe);
+			mvprintw(0, ownerNameLen, attacksThe);
+			attron(COLOR_PAIR(target->col));
+			mvprintw(0, ownerNameLen + attacksTheLen, "%s", target->name);
+			attroff(COLOR_PAIR(target->col));
+			int targetNameLen = strlen(target->name);
+			mvprintw(0, ownerNameLen + attacksTheLen + targetNameLen, " for %g hit points.\n", power - target->destructible->defense);
 		}
 		else
 		{
+			// failed to damage
 			mvprintw(
 				29, // y
 				0, // x
@@ -35,7 +52,8 @@ void Attacker::attack(Actor* owner, Actor* target)
 			);
 		}
 		
-		target->destructible->take_damage(target, power); // kills the player
+		// the target takes damage
+		target->destructible->take_damage(target, power); 
 		
 	}
 	else
