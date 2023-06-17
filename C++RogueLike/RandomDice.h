cafe7b2478c3b2c5a62853444f777867bd761775
autoexecbatman <91700559+autoexecbatman@users.noreply.github.com>
2023-06-17 20:11:45 +0300

    refactored random dice

--- C++RogueLike/RandomDice.h
+++ C++RogueLike/RandomDice.h
@@ -7,23 +7,21 @@
 class RandomDice
 {
 public:
-	int d4() { const int d4 = random_number_generator(1, 4); return d4; }
-	int d6() { const int d6 = random_number_generator(1, 6); return d6; }
-	int d8() { const int d8 = random_number_generator(1, 8); return d8; }
-	int d10() { const int d10 = random_number_generator(1, 10); return d10; }
-	int d12() { const int d12 = random_number_generator(1, 12); return d12; }
-	int d20() { const int d20 = random_number_generator(1, 20); return d20; }
-	int d100() { const int d100 = random_number_generator(1, 100); return d100; }
+    int d4() { return roll(4); }
+    int d6() { return roll(6); }
+    int d8() { return roll(8); }
+    int d10() { return roll(10); }
+    int d12() { return roll(12); }
+    int d20() { return roll(20); }
+    int d100() { return roll(100); }
 
-	int random_number_generator(int min, int max)
-	{
-		static std::random_device rd;
-		static std::mt19937 mt(rd());
-		std::uniform_int_distribution<int> dist(min, max);
+private:
+    std::mt19937 m_gen{ std::random_device{}() };
 
-		return dist(mt);
-	}
+    int roll(int max) {
+        return m_gen() % max + 1;
+    }
 };
 
 #endif // RANDOM_DICE_H
-// end of file: globals.h
+// end of file: RandomDice.h
