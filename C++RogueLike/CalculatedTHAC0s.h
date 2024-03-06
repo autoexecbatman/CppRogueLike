baf4ac55f8d2bc47715a4a3005cfed6d7eabcd81
Stanislav Kruk <insanebrainexperiment@hotmail.co.il>
2024-03-06 11:07:13 +0200

    refactor thaco getters to correctly access the vector

--- C++RogueLike/CalculatedTHAC0s.h
+++ C++RogueLike/CalculatedTHAC0s.h
@@ -9,8 +9,20 @@ struct CalculatedTHAC0s
 	std::vector<int> warrior{ 20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1 };
 	std::vector<int> wizard{ 20,20,20,19,19,19,18,18,18,17,17,17,16,16,16,15,15,15,14,14 };
 
-	int getFighter(int level) { return warrior[level]; }
-	int getRogue(int level) { return rogue[level]; }
-	int getCleric(int level) { return cleric[level]; }
-	int getWizard(int level) { return wizard[level]; }
+	int getFighter(int level) {
+		if (level > 0) { return warrior.at(level - 1); }
+		else { game.err("Your level is less then 1."); return 1; }
+	}
+	int getRogue(int level) {
+		if (level > 0) { return rogue.at(level - 1); }
+		else { game.err("Your level is less then 1."); return 1; }
+	}
+	int getCleric(int level) {
+		if (level > 0) { return cleric.at(level - 1); }
+		else { game.err("Your level is less then 1."); return 1; }
+	}
+	int getWizard(int level) {
+		if (level > 0) { return wizard.at(level - 1); }
+		else { game.err("Your level is less then 1."); return 1; }
+	}
 };
